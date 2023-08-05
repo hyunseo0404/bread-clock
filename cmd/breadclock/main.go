384a30f48a201e59a3f1822fd8c6898fae2f197d
hyunseo0404 <hyunseo0404@gmail.com>
2023-08-05 20:43:56 +0900

    Add logger (zap)

--- cmd/breadclock/main.go
+++ cmd/breadclock/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"go.uber.org/zap"
+	"log"
+	"os"
+	"syscall"
+)
 
+func init() {
+	// initialize logger
+	var logger *zap.Logger
+	if os.Getenv("BC_ENV") == "production" {
+		logger = zap.Must(zap.NewProduction())
+	} else {
+		logger = zap.Must(zap.NewDevelopment())
+	}
+	zap.ReplaceGlobals(logger)
+}
+
+func main() {
+	defer func() {
+		if err := zap.L().Sync(); err != nil && !errors.Is(err, syscall.ENOTTY) {
+			log.Printf("failed to sync logger: %v", err)
+		}
+	}()
 }
